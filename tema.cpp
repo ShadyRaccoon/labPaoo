c36a6a028f5e73e51b944d3a37e2d8fbf98f6426
ShadyRaccoon <marianbogdan953@yahoo.com>
2024-11-01 18:52:07 +0200

    demonstrare copy constructor

--- tema.cpp
+++ tema.cpp
@@ -2,6 +2,21 @@
 #include "CafeaComandata.h"
 
 int main(int argc, char** argv){
+    CafeaComandata c1(25, {"Lapte", "Cafea", "Pumpkin spice", "Scortisoara"}, "Serveste cu scortisoara extra", "Pumpkin Spice Latte");
+    std::cout << "Primul obj cafea" << std::endl;
+    c1.display();
 
+    //folosire copy constructor
+    CafeaComandata c2 = c1;
+    std::cout << "Copie nealterata" << std::endl;
+    c2.display();
+
+    //modificare copie si observarea starii obiectului copiat/originalului
+    c2.setCafea("Ceva de la Starbucks");
+    c2.setDiverse("Fara zahar");
+    std::cout << "Copia modificata:" << std::endl;
+    c2.display();
+    std::cout << "Originalul" << std::endl;
+    c1.display();
     return 0;
 }
