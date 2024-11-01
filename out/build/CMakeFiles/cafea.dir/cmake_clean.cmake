114369b25575a0a0a16b878e33e02e2dbfba2d94
ShadyRaccoon <marianbogdan953@yahoo.com>
2024-11-02 01:59:31 +0200

    adaugare fisiere necesare pentru build cu cmake

---
+++ out/build/CMakeFiles/cafea.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cafea.dir/CafeaComandata.cpp.o"
+  "CMakeFiles/cafea.dir/CafeaComandata.cpp.o.d"
+  "CMakeFiles/cafea.dir/tema.cpp.o"
+  "CMakeFiles/cafea.dir/tema.cpp.o.d"
+  "cafea"
+  "cafea.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cafea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
