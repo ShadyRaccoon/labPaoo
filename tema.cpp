5b80e21ebe343b1b19a711b3939b364caa78b27b
ShadyRaccoon <marianbogdan953@yahoo.com>
2024-11-02 02:00:37 +0200

    modificari mici in main si CMakeLists

--- tema.cpp
+++ tema.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
-#include "CafeaComandata.h"
+#include "CafeaComandata.hpp"
 
-int main(int argc, char** argv){
+void printLines(){
+    std::cout<<"--------------------------------------------------------------\n";
+}
+
+void demonstratie(){
     CafeaComandata c1(25, {"Lapte", "Cafea", "Pumpkin spice", "Scortisoara"}, "Serveste cu scortisoara extra", "Pumpkin Spice Latte");
     std::cout << "Primul obj cafea" << std::endl;
     c1.display();
+    printLines();
 
     //FOLOSIRE COPY CONSTRUCTOR
     CafeaComandata c2 = c1;
     std::cout << "Copie nealterata" << std::endl;
     c2.display();
+    printLines();
+
 
     //modificare copie si observarea starii obiectului copiat/originalului
     c2.setCafea("Ceva de la Starbucks");
     c2.setDiverse("Fara zahar");
     std::cout << "Copia modificata:" << std::endl;
     c2.display();
+    printLines();
+
     std::cout << "Originalul" << std::endl;
     c1.display();
+    printLines();
+
 
     //FOLOSIRE MOVE CONSTRUCTOR
     CafeaComandata c3 = std::move(c1);
     std::cout << "Obiectul mutat:" << std::endl;
     c3.display();
+    printLines();
 
     std::cout << "Obiectul original dupa mutare:" << std::endl;
     c1.display();
+    printLines();
+}
+
+int main(int argc, char** argv){
+    demonstratie();
 
     return 0;
-
 }
